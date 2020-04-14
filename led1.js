a5282e4eb103db86216eba3bcd66b87b5d9c7274
plm9606 <plm9606@naver.com>
2020-04-14 11:42:16 +0900

    feat: led on/off

---
+++ led1.js
@@ -0,0 +1,18 @@
+const gpio = require("node-wiring-pi");
+const LEDPIN = 29;
+let flag = 0;
+
+function timoutHandler() {
+  if (flag > 0) {
+    gpio.digitalWirte(LEDPIN, 1);
+    console.log("Node LED ON");
+  } else {
+    gpio.digitalWirte(LEDPIN, 0);
+    console.log("Node LED OFF");
+  }
+  setTimeout(timoutHandler, 1000);
+}
+
+gpio.setup("wpi");
+gpio.pinMode(LEDPIN, gpio.OUTPUT);
+setTimeout(timoutHandler, 1000);
