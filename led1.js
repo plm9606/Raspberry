360135c77ca9b80f0c0d173e931fe0d7e9ff931e
aram <plm9606@naver.com>
2020-04-14 11:56:57 +0900

    feat: flag 변경 함수 추가

--- led1.js
+++ led1.js
@@ -13,6 +13,15 @@ function timoutHandler() {
   setTimeout(timoutHandler, 1000);
 }
 
+function setFlag() {
+  if (flag) {
+    flag = 0;
+  } else {
+    flag = 1;
+  }
+  setTimeout(setFlag, 1000);
+}
 gpio.setup("wpi");
 gpio.pinMode(LEDPIN, gpio.OUTPUT);
+setFlag();
 setTimeout(timoutHandler, 1000);
