4878b25b4dc9a2c7b8eca71f06e08c1dd1fe6f8d
aram <plm9606@naver.com>
2020-04-14 14:21:17 +0900

    fix: 참조호출 수정

--- led_buzzer.js
+++ led_buzzer.js
@@ -6,11 +6,12 @@ function turnOnRed() {
   gpio.digitalWrite(BUZZER, 0);
   gpio.digitalWrite(RED, 1);
   console.log("LED on, Buzzer off");
-  setTimeout(turonOnRed, 1000);
+  setTimeout(turnOnBuzzer, 1000);
 }
 
 function turnOnBuzzer() {
   gpio.digitalWrite(BUZZER, 1);
+  gpio.digitalWrite(RED, 0);
   console.log("LED off, Buzzer on");
   setTimeout(turnOnRed, 100);
 }
