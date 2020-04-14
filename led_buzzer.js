485f18811ce784d761344b487443d7333cba4e7b
aram <plm9606@naver.com>
2020-04-14 14:18:38 +0900

    feat: buzzer와 led 동시 제어

---
+++ led_buzzer.js
@@ -0,0 +1,30 @@
+const gpio = require("node-wiring-pi");
+const BUZZER = 29;
+const RED = 28;
+
+function turnOnRed() {
+  gpio.digitalWrite(BUZZER, 0);
+  gpio.digitalWrite(RED, 1);
+  console.log("LED on, Buzzer off");
+  setTimeout(turonOnRed, 1000);
+}
+
+function turnOnBuzzer() {
+  gpio.digitalWrite(BUZZER, 1);
+  console.log("LED off, Buzzer on");
+  setTimeout(turnOnRed, 100);
+}
+
+process.on("SIGINT", function () {
+  gpio.digitalWrite(BUZZER, 0);
+  gpio.digitalWrite(RED, 0);
+  console.log("node All OFF");
+  process.exit();
+});
+
+// 초기화
+gpio.setup("wpi");
+gpio.pinMode(BUZZER, gpio.OUTPUT);
+gpio.pinMode(RED, gpio.OUTPUT);
+turnOnRed();
+turnOnBuzzer();
