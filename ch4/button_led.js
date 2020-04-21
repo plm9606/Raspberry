06369754086119a2bd66da9d5d4abcb434bebee4
plm9606 <plm9606@naver.com>
2020-04-21 11:32:05 +0900

    fix: 채터링해결을 위해 promise 사용

--- ch4/button_led.js
+++ ch4/button_led.js
@@ -4,15 +4,19 @@ const RED = 28;
 const BLUE = 25;
 let count = 0;
 
-function detectButton() {
+async function detectButton() {
   console.log(`Pressed! ${count}`);
-  if (count++ % 2 == 0) {
-    gpio.digitalWrite(RED, 1);
-    gpio.digitalWrite(BLUE, 0);
-  } else {
-    gpio.digitalWrite(RED, 0);
-    gpio.digitalWrite(BLUE, 1);
-  }
+  await new Promise((resolve, rej) => {
+    if (count++ % 2 == 0) {
+      gpio.digitalWrite(RED, 1);
+      gpio.digitalWrite(BLUE, 0);
+      resolve();
+    } else {
+      gpio.digitalWrite(RED, 0);
+      gpio.digitalWrite(BLUE, 1);
+      resolve();
+    }
+  });
 }
 
 process.on("SIGINT", function () {
