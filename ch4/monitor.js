1bb423be3090331090224a22b246924ba1d0d74a
aram <plm9606@naver.com>
2020-04-26 02:29:08 +0900

    fix: neopixel default RGB mode 변경

--- ch4/monitor.js
+++ ch4/monitor.js
@@ -17,7 +17,7 @@ let TRIGGER = false;
 
 let triggerTimer;
 
-ws281x.init({ count: NUM_LEDS, stripType: ws281x.WS2811_STRIP_RGB });
+ws281x.init({ count: NUM_LEDS, stripType: ws281x.WS2811_STRIP_GRB });
 ws281x.setBrightness(10);
 
 async function detectButton() {
