1a6c0e4a7f89f3436c3d2f1ebc80b599b7e1dbdc
plm9606 <plm9606@naver.com>
2020-05-21 14:03:14 +0900

    fix: adc.read()함수 호출 에러

--- midterm/ch5/ex1.js
+++ midterm/ch5/ex1.js
@@ -1,41 +1,37 @@
 const gpio = require("node-wiring-pi");
 const mcpadc = require("mcp-spi-adc");
-// const socket = require("socket.io");
-// const fs = require("fs");
-// const server = require("http").createServer((req, res) => {
-//   fs.readFile("views/test.html", "utf-8", (err, read) => {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.end();
-//   });
-// });
-// const io = socket.listen(server);
 
-let ANALOG_LIGHT;
-const speedHz = 1000000; //1Mhz
-const CS_MCP3208 = 10; // CE0 is set
-const LIGHT_CHANNEL = 0;
-let lightData;
+const CS_MCP3208 = 10, // CE0 is set
+  speedHz = 1000000, // colck speed = 1Mhz
+  LIGHT = 0; // adc 0번째 채널 선택 = 아날로그센서
+
+let timerId,
+  timeout = 800; // 타이머제어용
+let lightdata = -1; // 조도값 측정데이터 저장용
+
+const light = mcpadc.openMcp3208(LIGHT, { speedHz }, (err) => {
+  console.log(`SPI 채널0 초기화 완료`);
+  if (err) console.log(`채널 0 초기화 실패`);
+});
 
 gpio.wiringPiSetup();
 gpio.pinMode(CS_MCP3208, gpio.OUTPUT);
 
-const LIGHT = mcpadc.openMcp3208(LIGHT_CHANNEL, { speedHz }, (err) => {
-  console.log(`spi channel 0 초기화 완료`);
-  if (err) console.log(`spi channel 0 초기화 실패`);
-});
+const analogLight = () => {
+  light.read((err, reading) => {
+    console.log(`▲▽ ${reading.rawValue}`);
+    lightdata = reading.rawValue;
+  });
+  if (lightdata != -1) {
+    io.sockets.emit("watch", lightdata);
+
+    console.log(`${(lightdata / 4095) * 100}%`);
+
+    timerId = setTimeout(analogLight, timeout);
+  }
+};
 
 process.on("SIGINT", () => {
+  console.log(`MCP-ADC가 해제되어, 웹서버를 종료합니다`);
   process.exit();
 });
-
-(function analogLight() {
-  LIGHT.read((err, reading) => {
-    console.log(
-      `rawVal: ${reading.rawValue}, ${(reading.rawValue / 4095) * 100}%`
-    );
-    lightData = reading.rawValue;
-  });
-
-  //   socket.emit();
-  ANALOG_LIGHT = setTimeout(analogLight, 1000);
-})();
